e4e78818409bd67bdff5ba1dcb8a9a2edf004fec
gon23 <gon23>
2004-04-15 10:18:51 +0000

    Replaced commons-cli with JSAP

--- src/wol/WakeOnLan.java
+++ src/wol/WakeOnLan.java
@@ -1,35 +1,31 @@
 /*
- * $Id: WakeOnLan.java,v 1.4 2004/04/14 18:21:40 gon23 Exp $
+ * $Id: WakeOnLan.java,v 1.5 2004/04/15 10:22:06 gon23 Exp $
  */
 package wol;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.net.InetAddress;
 import java.util.logging.Logger;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
+import com.martiansoftware.jsap.FlaggedOption;
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.Switch;
+import com.martiansoftware.jsap.UnflaggedOption;
+import com.martiansoftware.jsap.stringparsers.InetAddressStringParser;
+import com.martiansoftware.jsap.stringparsers.IntegerStringParser;
+import com.martiansoftware.jsap.stringparsers.StringStringParser;
 
 import wol.configuration.Configuration;
+import wol.resources.Messages;
 import wol.ui.MainPanel;
 
 /**
@@ -44,85 +40,44 @@ public class WakeOnLan {
 	private final static String VERSIONSTRING = "wakeonlan version " + VERSION;
 	private final static Logger LOG = Logger.getLogger(WakeOnLan.class.getName());
 
-	private ResourceBundle bundle = ResourceBundle.getBundle("wol.Resources");
-	private Options options;
+	private static final String CMD_INET_ADDRESS = "inet-address";
+	private static final String CMD_PORT = "port";
+	private static final String CMD_CONFIG_FILE = "config";
+	private static final String CMD_VERSION = "verbose";
+	private static final String CMD_HELP = "help";
+	private static final String CMD_DEBUG = "debug";
+	private static final String CMD_ADDRESSES = "addresses";
 	
-	public WakeOnLan(String args[]) {
+	public WakeOnLan(JSAPResult config) throws JSAPException, IOException {
 		super();
 		
-		createOptions();
-
-		CommandLineParser parser = new PosixParser();
-		String host = WakeUpUtil.DEFAULT_HOST;
-		int port = WakeUpUtil.DEFAULT_PORT;
-		boolean debug = false;
+		if (!config.success()) {
+			printHelpAndExit();
+		}
 		
-		try {
-			CommandLine line = parser.parse(options, args);
-			
-			debug = line.hasOption('d'); 
-			
-			if (line.hasOption('h')) {
-				printHelpAndExit();
-			}
-			
-			if (line.hasOption('v')) {
-				printVersionAndExit();
-			}
-			
-			if (line.hasOption('p')) {
-				port = Integer.parseInt(line.getOptionValue('p'));	
-			}
-			
-			if (line.hasOption('i')) {
-				host = line.getOptionValue('i');				
-			}
-			
-			String[] hardwareAddresses; 
-			
-			if (line.hasOption('f')) {
-				List hwAddressList = new ArrayList();
-				String file = line.getOptionValue('f');
-				try {
-					BufferedReader reader = new BufferedReader(new FileReader(file));
-					
-					for (String address = reader.readLine(); null != address; address = reader.readLine()) {
-						hwAddressList.add(address);
-					}
-				} catch (Exception e) {
-					printErrorAndExit(MessageFormat.format(bundle.getString("error.inputfile"), new Object[]{file}), e, debug);
-				}
-				
-				hardwareAddresses = (String[]) hwAddressList.toArray(new String[hwAddressList.size()]);
-			} else {
-				hardwareAddresses = line.getArgs();
-			}
-			
-			if (null == hardwareAddresses || 0 == hardwareAddresses.length) {
-				showGUI();
-			} else {
-				WakeUpUtil wakeOnLan = new WakeUpUtil(host, port);
-			
-				wakeOnLan.wakeUp(hardwareAddresses);
-			}
-			
-							
-		} catch (ParseException e) {
-			printErrorAndExit(bundle.getString("error.parsingCommandline"), e, debug);
-			System.exit(1);
-		} catch (UnknownHostException e) {
-			printErrorAndExit(MessageFormat.format(bundle.getString("error.unknownHost"), new Object[] {host}), e, debug);
-		} catch (SocketException e) {
-			printErrorAndExit(bundle.getString("error.socket"), e, debug);
-		} catch (IOException e) {
-			printErrorAndExit(MessageFormat.format(bundle.getString("error.io"), new Object[] {host, new Integer(port)}), e, debug);
-		} catch (SecurityException e) {
-			printErrorAndExit(bundle.getString("error.security"), e, debug);
-		} catch (NumberFormatException e) {
-			printErrorAndExit(MessageFormat.format(bundle.getString("error.portNaN"), new Object[]{new Integer(port)}),  e, debug);
+		if (config.getBoolean(CMD_HELP)) {
+			printHelpAndExit();
+		}
+		
+		if (config.getBoolean(CMD_VERSION)) {
+			printVersionAndExit();
+		}
+		
+		int port = config.getInt(CMD_PORT);
+		InetAddress host = config.getInetAddress(CMD_INET_ADDRESS);
+		
+		String[] hardwareAddresses = config.getStringArray(CMD_ADDRESSES); 
+		
+		
+		if (null == hardwareAddresses || 0 == hardwareAddresses.length) {
+			showGUI();
+		} else {
+			WakeUpUtil wakeOnLan = new WakeUpUtil(host, port);
+		
+			wakeOnLan.wakeUp(hardwareAddresses);
 		}
 	}
-
+	
 	private void showGUI() {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -156,8 +111,11 @@ public class WakeOnLan {
 	 * 
 	 * @param args an array containing the commandline arguments, if any
 	 */
-	public static void main(String[] args) {
-		new WakeOnLan(args);
+	public static void main(String[] args) throws JSAPException, IOException {
+		JSAP jsap = getOptions();
+		JSAPResult config = jsap.parse(args);
+
+		new WakeOnLan(config);
 	}
 	
 	/**
@@ -165,36 +123,15 @@ public class WakeOnLan {
 	 * 
 	 * @param exitCode the exit code
 	 */
-	protected void printHelpAndExit(int exitCode) {
-		HelpFormatter formatter = new HelpFormatter();
-		StringBuffer buffer = new StringBuffer();
+	protected void printHelpAndExit(int exitCode) throws JSAPException {
+		JSAP jsap = getOptions();
 		
-		buffer.append(bundle.getString("usage.cmdline.label"));
-		buffer.append(' ');
-		buffer.append(bundle.getString("usage.option.label"));
-		buffer.append(' ');
-		buffer.append(bundle.getString("usage.hardwareAddress.label"));
-		
-		formatter.printHelp(buffer.toString(), bundle.getString("help.header"), options, bundle.getString("help.footer"), false);
+		System.out.println();
+		System.out.println("Usage: " + WakeOnLan.class.getName());
+		System.out.println(jsap.getUsage());
+		System.out.println();
+		System.out.println(jsap.getHelp());
 		System.exit(exitCode);
-	}
-	
-	/**
-	 * Prints an error and exits with code 1.
-	 * 
-	 * @param err an error message
-	 * @param e the exception the caused the error. can be null
-	 * @param debug if true, the exception will be printetd too
-	 */
-	protected void printErrorAndExit(String err, Exception e, boolean debug) {
-		System.err.println(err);
-		
-		if (debug && null != e) {
-			System.err.println("Exception:\n");
-			System.err.println(e.getLocalizedMessage());
-		}
-		
-		System.exit(1);
 	}
 	
 	/**
@@ -210,42 +147,79 @@ public class WakeOnLan {
 	 * Prints the commandline help and exits with code 0.
 	 *
 	 */
-	protected void printHelpAndExit() {
+	protected void printHelpAndExit() throws JSAPException {
 		printHelpAndExit(0);
 	}
 	
-	/**
-	 * Creates the commandline options
-	 *
-	 */
-	private void createOptions() {
-		options = new Options();
-		OptionBuilder.withArgName(bundle.getString("cmdline.value.inet-address"));
-		OptionBuilder.withLongOpt("inet-address");
-		OptionBuilder.hasArg();
-		OptionBuilder.withDescription(bundle.getString("cmdline.description.inet-address"));
-		options.addOption(OptionBuilder.create('i'));
+	protected static JSAP getOptions() throws JSAPException {
+		JSAP jsap = new JSAP();
 		
-		OptionBuilder.withArgName(bundle.getString("cmdline.value.port"));
-		OptionBuilder.withLongOpt("port");
-		OptionBuilder.hasArg();
-		OptionBuilder.withDescription(bundle.getString("cmdline.description.port"));
-		options.addOption(OptionBuilder.create('p'));
+		FlaggedOption inetAdress = new FlaggedOption(CMD_INET_ADDRESS)
+			.setStringParser(new InetAddressStringParser())
+			.setDefault("255.255.255.255")
+			.setRequired(false)
+			.setShortFlag('i')
+			.setLongFlag("inet-address");
 		
-		OptionBuilder.withArgName(bundle.getString("cmdline.value.file"));
-		OptionBuilder.withLongOpt("file");
-		OptionBuilder.hasArg();
-		OptionBuilder.withDescription(bundle.getString("cmdline.description.file"));
-		options.addOption(OptionBuilder.create('f'));
+		inetAdress.setHelp(Messages.CMD_MESSAGES.getString("cmdline.description.inet-address"));
+		jsap.registerParameter(inetAdress);
 		
-		options.addOption("v", "version", false, bundle.getString("cmdline.description.version"));
-		options.addOption("h", "help", false, bundle.getString("cmdline.description.help"));
-		options.addOption("d", "debug", false, bundle.getString("cmdline.description.debug"));
+		FlaggedOption port = new FlaggedOption(CMD_PORT)
+			.setStringParser(new IntegerStringParser())
+			.setDefault("9")
+			.setRequired(false)
+			.setShortFlag('p')
+			.setLongFlag("port");
+		
+		port.setHelp(Messages.CMD_MESSAGES.getString("cmdline.description.port"));
+		jsap.registerParameter(port);
+		
+		FlaggedOption config = new FlaggedOption(CMD_CONFIG_FILE)
+			.setStringParser(new StringStringParser())
+			.setRequired(false)
+			.setShortFlag('c')
+			.setLongFlag("config");
+		
+		config.setHelp(Messages.CMD_MESSAGES.getString("cmdline.description.config"));
+		jsap.registerParameter(config);
+		
+		Switch version = new Switch(CMD_VERSION)
+			.setShortFlag('v')
+			.setLongFlag("version");
+		
+		version.setHelp(Messages.CMD_MESSAGES.getString("cmdline.description.version"));
+		jsap.registerParameter(version);
+		
+		Switch help = new Switch(CMD_HELP)
+			.setShortFlag('h')
+			.setLongFlag("help");
+		
+		help.setHelp(Messages.CMD_MESSAGES.getString("cmdline.description.help"));
+		jsap.registerParameter(help);
+		
+		Switch debug = new Switch(CMD_DEBUG)
+			.setShortFlag('d')
+			.setLongFlag("debug");
+		
+		debug.setHelp(Messages.CMD_MESSAGES.getString("cmdline.description.debug"));
+		jsap.registerParameter(debug);
+		
+		UnflaggedOption addresses = new UnflaggedOption(CMD_ADDRESSES)
+			.setRequired(false)
+			.setGreedy(true);
+		
+		addresses.setHelp(Messages.CMD_MESSAGES.getString("cmdline.description.addresses"));
+		jsap.registerParameter(addresses);
+	
+	return jsap;
 	}
 }
 
 /*
  * $Log: WakeOnLan.java,v $
+ * Revision 1.5  2004/04/15 10:22:06  gon23
+ * Replaced commons-cli with JSAP
+ *
  * Revision 1.4  2004/04/14 18:21:40  gon23
  * *** empty log message ***
  *
