fd32ad4a889e50fe1edeb558e0e3f4611163a1b1
gon23 <gon23>
2004-05-18 16:06:43 +0000

    *** empty log message ***

--- etc/wakeonlan.bat
+++ etc/wakeonlan.bat
@@ -1,1 +1,11 @@
-javaw -jar wakeonlan.jar
+@echo off
+rem Stolen from tomcat
+set CMD_LINE_ARGS=
+:setArgs
+if ""%1""=="""" goto doneSetArgs
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setArgs
+:doneSetArgs
+
+javaw -jar wakeonlan.jar %CMD_LINE_ARGS%
--- etc/wakeonlan.sh
+++ etc/wakeonlan.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar wakeonlan.jar
+java -jar wakeonlan.jar "$@"
